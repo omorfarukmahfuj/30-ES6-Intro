50564c6374fecf1f236103e2738e83cc7c95ecf3
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-04-04 07:30:56 +0600

    practice-problem-01 added

---
+++ Practice Problem/practice-problem-01.js
@@ -0,0 +1,14 @@
+// Array Function
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+console.log(multiply(2, 3, 4));
+
+// Template Literals
+const stringTemplate = `I am a developer.
+I love to code.
+I love dark mode on screen.
+`;
+console.log(stringTemplate);
+
+// Default value of arrow function
+const sum = (num1 = 0, num2 = 0) => num1 + num2;
+console.log(sum(2));
